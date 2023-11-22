15ca013aed38bef1a76e25d13f3331f3600e4dfa
Nazar Nyrka <nazarnyrka00@gmail.com>
2023-11-22 08:53:54 -0500

    index updated

--- index.js
+++ index.js
@@ -1,138 +1,115 @@
 const express = require('express');
 const fs = require('fs');
-const path = require('path');
 const multer = require('multer');
-const upload = multer();
 
+const upload = multer();
 const app = express();
 
-app.use(express.text());
+const port = 8000;
+const notesFilePath = 'notes.json';
+
+try {
+  const fileContent = fs.readFileSync(notesFilePath, 'utf8');
+  if (fileContent.trim() === '') {
+      fs.writeFileSync(notesFilePath, '[]', 'utf8');
+  }
+} catch (error) {
+  //console.error('Помилка при роботі з файлом notes.json:', error);
+}
+
 app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
-app.use('/', express.static('static'));
 
-//#region Endpoints
-app.get('/notes', getNotesHandler);
-app.get('/UploadForm.html', getUploadForm);
-app.post('/upload', upload.single('note'), uploadNoteHandler);
-app.get('/notes/:noteName', getNoteByName);
-app.put('/notes/:noteName', express.json(), updateNoteByName);
-app.delete('/notes/:noteName', deleteNoteByName);
-
-app.get('/downloadNotes', (req, res) => {
-  const filePath = path.join(__dirname, 'notes.json');
-  res.download(filePath, 'notes.json', (err) => {
-    if (err) {
-      res.status(404).send('File not found!');
-    }
-  });
+app.get('/', (req, res) => {
+  res.send('Сервіс запущено. Вітаємо!');
 });
-//#endregion
 
-//#region Handlers
-function getNotesHandler(req, res) {
-  const jsonData = getJsonData();
-  res.json(jsonData);
-}
+app.get('/UploadForm.html', (req, res) => {
+  res.sendFile(__dirname + '/static/UploadForm.html');
+});
 
-function getUploadForm(req, res) {
-  const formPath = path.join(__dirname, 'UploadForm.html');
-  res.sendFile(formPath);
-}
-
-function uploadNoteHandler(req, res) {
-  const { note_name, note } = req.body;
-
-  const existingNote = checkNoteExists(note_name);
-
-  if (existingNote) {
-    return res.status(400).send('Note with that name already exists.');
-  }
-
-  const jsonData = getJsonData();
-
-  jsonData.push({ note_name, note });
-  fs.writeFileSync('notes.json', JSON.stringify(jsonData));
-
-  res.status(201).send(req.body);
-}
-
-function getNoteByName(req, res) {
-  const noteName = req.params.noteName;
-  const note = getNoteContent(noteName);
-
-  if (!note) {
-    return res.status(404).send('Note not found.');
-  }
-
-  res.send(note);
-}
-
-function updateNoteByName(req, res) {
-  const noteName = req.params.noteName;
-  const note = req.body;
-
-  const jsonData = getJsonData();
-
-  const existingNoteIndex = jsonData.findIndex(
-    (item) => item.note_name === noteName
-  );
-
-  if (existingNoteIndex === -1) {
-    return res.status(404).send('Note not found.');
-  }
-
-  jsonData[existingNoteIndex].note = note;
-  fs.writeFileSync('notes.json', JSON.stringify(jsonData));
-
-  res.send('Note updated successfully.');
-}
-
-function deleteNoteByName(req, res) {
-  const noteName = req.params.noteName;
-  const jsonData = getJsonData();
-
-  const updatedNotes = jsonData.filter(
-    (item) => item.note_name !== noteName
-  );
-
-  if (updatedNotes.length === jsonData.length) {
-    return res.status(404).send('Note not found.');
-  }
-
-  fs.writeFileSync('notes.json', JSON.stringify(updatedNotes));
-
-  res.send('Note deleted successfully.');
-}
-//#endregion
-
-//#region Helpers
-function checkNoteExists(noteName) {
-  const jsonData = getJsonData();
-  return jsonData.find((note) => note.note_name === noteName);
-}
-
-function getJsonData() {
-  if (fs.existsSync('notes.json')) {
-    const fileData = fs.readFileSync('notes.json', 'utf8');
-    if (fileData.trim() !== '') {
-      try {
-        return JSON.parse(fileData);
-      } catch (error) {
-        console.error('Invalid JSON format in notes.json');
-      }
+app.get('/notes', (req, res) => {
+    try {
+      const notes = JSON.parse(fs.readFileSync(notesFilePath, 'utf8'));
+      res.json(notes);
+    } catch (error) {
+      console.error('Помилка при отриманні нотаток:', error);
+      res.status(500).json([]);
     }
+});
+
+app.get('/notes/:noteName', (req, res) => {
+    const noteName = req.params.noteName;
+
+    if (noteName.trim() === '') {
+        alert('Будь ласка, введіть назву нотатки.');
+        
+    }
+
+    const notes = JSON.parse(fs.readFileSync(notesFilePath, 'utf8'));
+    const note = notes.find((n) => n.note_name === noteName);
+
+    if (note) {
+        res.send(note.note);
+    } else {
+        res.status(404).send('Not found'); 
+    }
+});
+
+app.post('/upload', upload.none(), (req, res) => {
+  const noteName = req.body.note_name;
+  const noteText = req.body.note;
+
+  // Перевірка наявності файлу notes.json
+  let notes = [];
+  if (fs.existsSync(notesFilePath)) {
+      // Якщо файл існує, зчитуємо його вміст
+      notes = JSON.parse(fs.readFileSync(notesFilePath, 'utf8'));
   }
-  return [];
-}
 
-function getNoteContent(noteName) {
-  const jsonData = getJsonData();
-  const note = jsonData.find((item) => item.note_name === noteName);
-  return note ? note.note : null;
-}
-//#endregion
+  if (notes.some((note) => note.note_name === noteName)) {
+      res.status(400).send('Помилковий запит: Нотатка з такою назвою вже існує.');
+  } else {
+      const newNote = { note_name: noteName, note: noteText };
+      notes.push(newNote);
 
-app.listen(8000, () => {
-  console.log('Server is running on port 8000');
+      // Записуємо оновлений масив у файл notes.json
+      fs.writeFileSync(notesFilePath, JSON.stringify(notes), 'utf8');
+      res.status(201).send('Створено');
+  }
+});
+
+app.put('/notes/:noteName',express.text(), (req, res) => {
+  const noteName = req.params.noteName;
+  const UpdatenoteText = req.body;
+
+  if (noteName.trim() === '') {
+   return res.status(400).send('Будь ласка, введіть назву нотатки.');
+    
+}
+  const notes = JSON.parse(fs.readFileSync(notesFilePath, 'utf8'));
+  const noteUpdate = notes.find((n) => n.note_name === noteName);
+  if (noteUpdate) {
+    noteUpdate.note = UpdatenoteText;
+    fs.writeFileSync(notesFilePath, JSON.stringify(notes), 'utf8');
+    res.status(200).send('OK');
+  } else {
+    res.status(404).send('Не знайдено');
+  }
+});
+
+app.delete('/notes/:noteName', (req, res) => {
+  const noteName = req.params.noteName;
+  const notes = JSON.parse(fs.readFileSync(notesFilePath, 'utf8'));
+  const noteIndex = notes.findIndex((n) => n.note_name === noteName);
+  if (noteIndex !== -1) {
+    notes.splice(noteIndex, 1);
+    fs.writeFileSync(notesFilePath, JSON.stringify(notes), 'utf8');
+    res.status(200).send('OK');
+  } else {
+    res.status(404).send('Не знайдено');
+  }
+});
+
+app.listen(port, () => {
+  console.log(`Сервер працює на порту ${port}`);
 });
